dc08e068c775c8fddcefe85f273ba0e7b8d7b822
SaTarantino <salvatoretrn@gmail.com>
2019-11-26 12:41:03 +0000

    Almost fix quick sort

--- src/arraySorter/QuickSort.java
+++ src/arraySorter/QuickSort.java
@@ -7,16 +7,7 @@ public class QuickSort<T extends Comparable<? super T>> implements ArraySort<T> 
     public T[] sort(T[] array) {
 
         System.out.println(Arrays.toString(array));
-
-
-        if (array[0].compareTo(array[array.length - 1]) < 0) {
-
-            int pi = quickSort(array, 0, array.length - 1);
-
-            quickSort(array, 0, pi-1);
-            quickSort(array, pi+1, array.length - 1);
-
-        }
+        int pi = quickSort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
         return array;
 
@@ -24,22 +15,22 @@ public class QuickSort<T extends Comparable<? super T>> implements ArraySort<T> 
 
     private int quickSort (T[] array, int low, int high) {
 
-        int pivot = array.length - 1;
-        int i = low - 1;
+        int pivot = high;
+        int i = low;
         T tmp;
 
         for (int j = low; j < high; j++) {
             if (array[j].compareTo(array[pivot]) < 0) {
-                i++;
                 tmp = array[i];
                 array[i] = array[j];
                 array[j] = tmp;
+                i++;
             }
         }
-        tmp = array[i + 1];
-        array[i + 1] = array[high];
+        tmp = array[i];
+        array[i] = array[high];
         array[high] = tmp;
 
-        return i + 1;
+        return i;
     }
 }
